ff0344204bb7797e90bcef67423ee887c5c751d7
Dan Zakirov <alexodiy@gmail.com>
2022-09-30 19:24:53 +0300

    Различные исправления

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 const mix = require('laravel-mix');
 
-
-const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const imageminMozjpeg = require('imagemin-mozjpeg');
@@ -46,40 +44,6 @@ mix
     // Настраиваем webpack для обработки изображений
     .webpackConfig({
         plugins: [
-            // Создаем svg-спрайт с иконками
-            new SVGSpritemapPlugin(
-                'resources/assets/images/icons/*.svg', // Путь относительно каталога с webpack.mix.js
-                {
-                    output: {
-                        filename: 'public/assets/images/icons.svg', // Путь относительно каталога public/
-                        svg4everybody: false, // Отключаем плагин "SVG for Everybody"
-                        svg: {
-                            sizes: false // Удаляем инлайновые размеры svg
-                        },
-                        chunk: {
-                            keep: true, // Включаем, чтобы при сборке не было ошибок из-за отсутствия spritemap.js
-                        },
-                        svgo: {
-                            plugins: [
-                                {
-                                    'removeStyleElement': false // Удаляем из svg теги <style>
-                                },
-                                {
-                                    'removeAttrs': {
-                                        attrs: ["fill", "stroke"] // Удаляем часть атрибутов для управления стилями из CSS
-                                    }
-                                },
-                            ]
-                        },
-                    },
-                    sprite: {
-                        prefix: 'icon-', // Префикс для id иконок в спрайте, будет иметь вид 'icon-имя_файла_с_иконкой'
-                        generate: {
-                            title: false, // Не добавляем в спрайт теги <title>
-                        },
-                    },
-                }
-            ),
             // Копируем картинки из каталога ресурсов в публичный каталог
             new CopyWebpackPlugin({
                 patterns: [
