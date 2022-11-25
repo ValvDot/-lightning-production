e2b70f17783addaf418e62c3c2066f21ecf39814
Dan Zakirov <alexodiy@gmail.com>
2022-11-25 22:53:15 +0300

    Фонарь

--- resources/assets/js/glide-init.js
+++ resources/assets/js/glide-init.js
@@ -35,6 +35,46 @@ if (carouselGeneral) {
             }
         });
 
+
+        //carouselGeneral.i.on("swipe.start", function (event) {
+
+        carouselGeneral.i.on("run.after", function (event) {
+
+            console.log('111111');
+
+            carouselGeneral.i.selector.querySelector('.air-shadow').remove();
+            carouselGeneral.i.selector.querySelector('.glide__slide--active').querySelector('.air-bg').insertAdjacentHTML('afterend', '<div class="air-shadow"></div>');
+
+            const airShadow = document.querySelector('.air-shadow');
+
+            let spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 600px)';
+
+            window.addEventListener('mousemove', e => updateSpotlight(e));
+
+            window.addEventListener('mousedown', e => {
+
+                spotlightSize = 'transparent 130px, rgba(0, 0, 0, 0.95) 450px)';
+
+                updateSpotlight(e);
+
+            });
+
+            window.addEventListener('mouseup', e => {
+
+                spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 600px)';
+
+                updateSpotlight(e);
+
+            });
+
+            function updateSpotlight(e) {
+
+                airShadow.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+
+            }
+
+        });
+
         carouselGeneral.i.mount();
     }
 }
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,3 +1,36 @@
+//DOM load event
+window.addEventListener("DOMContentLoaded", () => {
+
+    const airShadow = document.querySelector('.airShadow');
+
+    let spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 600px)';
+
+    window.addEventListener('mousemove', e => updateSpotlight(e));
+
+    window.addEventListener('mousedown', e => {
+
+        spotlightSize = 'transparent 130px, rgba(0, 0, 0, 0.95) 450px)';
+
+        updateSpotlight(e);
+
+    });
+
+    window.addEventListener('mouseup', e => {
+
+        spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 600px)';
+
+        updateSpotlight(e);
+
+    });
+
+    function updateSpotlight(e) {
+
+        airShadow.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+
+    }
+
+});
+
 $(function () {
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
